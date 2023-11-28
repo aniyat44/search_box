e58c06c772adc332cb5560f92f6e79f4ad6092ba
aniyat44 <142137045+aniyat44@users.noreply.github.com>
2023-11-28 10:21:03 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.search_btn').on("click", ()=>{
+        $('#search').show('open');
+    });
+    $('.close_btn').on("click", ()=>{
+        $('#search').hide('open');
+    });
+});
